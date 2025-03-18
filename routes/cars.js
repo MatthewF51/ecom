ebac499c661413bf28651d2911d89e1fb18b295b
CJ <113473673+C0REC0N@users.noreply.github.com>
2025-03-18 00:10:19 +0000

    where hope

--- routes/cars.js
+++ routes/cars.js
@@ -31,10 +31,10 @@ router.get('/:id', async (req, res) => {
 });
 
 // Route: Fetch cars by query
-router.get('/query', async (req, res) => {
+router.post('/query', async (req, res) => {
   try {
-    const { query } = req.params;
-    const result = await pool.query('$query',[query]);
+    const { search } = req.body;
+    const result = await pool.query(search);
     if (result.rows.length === 0) {
       res.status(404).json({ error: 'Car not found' });
     } else {
