e9dbbcf3d0f64e9c4211b8f93ea22e2e095da47c
CJ <cp2020@hw.ac.uk>
2025-03-20 08:03:23 +0000

    Update update.js

--- routes/update.js
+++ routes/update.js
@@ -5,8 +5,22 @@ const pool = require('../db');
 // Route: Fetch a single car by ID
 router.post('/insert', async (req, res) => {
   const { user, carId, type, attributes } = req.body;
-
-	res.redirect(`/`);
+  try {
+	   
+    const result = await pool.query(`SELECT email FROM users`);
+	
+	if (result.rows.length === 0) {
+		console.error('Error updating');
+	  res.status(404).json({ error: 'Car not found' });
+	} else {
+	  res.json(upResult.rows[0]);
+	}
+	res.redirect(`/carDetails.html?user=${user}&id=${carId}`);
+  } catch (err) {
+    console.error('Error fetching car details:', err);
+    res.status(500).json({ error: 'Internal server error' });
+	res.redirect(`/carDetails.html?user=${user}&id=${carId}`);
+  }
 });
 
 module.exports = router;
